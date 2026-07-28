4b3f063ed1aa308eeaf6aabe24cc2cdc9778523e
Paula Margon <martingc.paula@gmail.com>
2026-07-28 07:21:46 +0100

    nuevos ejercicios

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-int alturaEnCentimetros;
-alturaEnCentimetros = 180;
+namespace Variables.Ejercicios
+{
+    /*
+    En este problema vas a declarar e inicializar una variable de tipo entero. Cuando hablamos de inicializar nos referimos a asignarle un valor.
 
-Console.WriteLine(alturaEnCentimetros);
+    Entonces, debajo del comentario, pero encima del return, debes:
+
+        1) Declarar una variable de tipo int y nombre elSignificadoDeLaVida
+
+        2) Asígnale el valor 42
+
+    Remueve las dos líneas indicadas más abajo para descomentar el código y comenzar el ejercicio.
+     */
+
+    public class NumerosEnteros
+    {
+
+        public int Ejercicio()
+        {
+            // Tu código debajo de esta línea
+
+            int elSignificadoDeLaVida = 42;
+
+            return elSignificadoDeLaVida;
+        }
+
+    }
+}
