20396ddd0b8f01e9aaf0005ea67b8eca26d3574f
Paula Margon <martingc.paula@gmail.com>
2026-08-11 08:44:49 +0100

    Ordenado por archivos diferentes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,58 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleApp1;
 
-namespace Variables.Ejercicios
-{
-    /* EJERCICIO
-    En este problema vas a declarar e inicializar una variable de tipo entero. Cuando hablamos de inicializar nos referimos a asignarle un valor.
+Level3.Ejecutar();
 
-    Entonces, debajo del comentario, pero encima del return, debes:
+//PLANTILLA
 
-        1) Declarar una variable de tipo int y nombre elSignificadoDeLaVida
 
-        2) Asígnale el valor 42
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-    Remueve las dos líneas indicadas más abajo para descomentar el código y comenzar el ejercicio.
-     */
-
-    public class NumerosEnteros
-    {
-
-        public int Ejercicio()
-        {
-            // Tu código debajo de esta línea
-
-            int elSignificadoDeLaVida = 42;
-
-            return elSignificadoDeLaVida;
-        }
-
-    }
-}
-
-/*
- * EJERCICIO
-En este problema vas a declarar e inicializar una variable de tipo DateTime con una fecha y hora específica.
-
-Entonces, debajo del comentario, pero encima del return, debes:
-
-1) Declarar una variable de tipo DateTime y nombre fecha
-
-2) Asignarle la fecha 12 de mayo a las 10 de la mañana. El año puede ser cualquiera (puedes poner el año actual).
-
-Remueve las dos líneas indicadas más abajo para descomentar el código y comenzar el ejercicio.
-
-DateTime fecha = new DateTime(2026, 5, 12, 10, 0, 0);
-*/
-
-var cantidad1 = 7;
-var cantidad2 = 4;
-var resultadoSuma = cantidad1 + cantidad2;
-var resultadoResta = cantidad1 - cantidad2;
-
-Console.WriteLine("La suma es " + resultadoSuma);
-Console.WriteLine($"La suma de {cantidad1} y {cantidad2} es igual a: {resultadoSuma")
+//namespace ConsoleApp1
+//{
+//    internal class Level3
+//    {
+//        public static void Ejecutar()
+//        {
+//            Console.WriteLine("Hola");
+//        }
+//    }
+//}
---
+++ ConsoleApp1/ConsoleApp1/level3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Level3
+    {
+        public static void Ejecutar()
+        {
+            //EJERCICIO//
+            //            Estas tierras llevan mucho tiempo sin ver programación debido a la plaga del Sin Codificar.Incluso los viejos
+            //programas se están desmoronando ahora.Tus habilidades con la programación son aún incipientes, pero puedes
+            //marcar la diferencia en la vida de estas personas.Quizá algún día, en un futuro próximo, tus habilidades se hayan fortalecido
+            //lo suficiente como para enfrentarte directamente al Sin Codificar. Pero, por ahora, decides hacer lo que puedas para ayudar.
+            //En la cercana ciudad de Consolas, la comida escasea. Telim tiene un horno mágico capaz de producir pan de
+            //la nada. Está dispuesto a compartirlo, pero Telim es un exceliano, y a los excelianos les encanta el papeleo; lo exigen para
+            //todas las transacciones, sin excepciones.Telim compartirá su pan con la ciudad si consigues crear un programa que
+            //le permita introducir los nombres de quienes lo recibirán. Una ejecución de prueba de este programa tiene este aspecto:
+            //El pan está listo.
+            //¿Para quién es el pan?
+            //RB
+            //Anotado: RB ha recibido pan.
+
+            Console.WriteLine("El pan está listo");
+            Console.WriteLine("¿Para quién es el pan?");
+            var Nombre = Console.ReadLine();
+            Console.WriteLine("Anotado: " + Nombre + " ha recibido pan");
+
+
+        }
+    }
+}
